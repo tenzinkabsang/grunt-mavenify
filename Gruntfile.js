e682d605b1bea340afa6c4ee5d526235af502cdf
Tenzin Kabsang <tkabsang@ancestry.com>
2014-10-02 16:05:53 -0600

    added shelljs and watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,74 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    // measures the time each task takes
+    require('time-grunt')(grunt);
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+    // load grunt config
+    require('load-grunt-tasks')(grunt);
 
-    // Configuration to be run (and then tested).
-    mavenify: {
-      default_options: {
-        options: {
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                '<%= nodeunit.tests %>'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
         },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+
+        // Configuration to be run (and then tested).
+        mavenify: {
+            default_options: {
+                options: {
+                },
+                files: {
+                    'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+                }
+            },
+            custom_options: {
+                options: {
+                    separator: ': ',
+                    punctuation: ' !!!'
+                },
+                files: {
+                    'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+                }
+            }
+        },
+
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js']
         }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+    });
 
-  });
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'mavenify', 'nodeunit']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['clean', 'mavenify', 'nodeunit']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/mavenify.js
+++ tasks/mavenify.js
@@ -8,22 +8,36 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+var shell = require('shelljs');
+var watch = require('watch');
 
-    function cmdToRun(target){
+module.exports = function (grunt) {
+
+    // measures the time each task takes
+    require('time-grunt')(grunt);
+
+    function cmdToRun(target) {
         var options = target.options();
 
-        if(options.cmd === ''){
+        if (options.cmd === '') {
             return 'mvn test -q';
         }
         return options.quite ? options.cmd + ' -q' : options.cmd;
     }
 
-  grunt.registerMultiTask('mavenify', 'run maven goals through grunt', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-        quite: true,
-        cmd: ''
+    grunt.registerMultiTask('mavenify', 'run maven goals through grunt', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        this.options({
+            quite: true,
+            cmd: ''
+        });
+
+        var finalCmd = cmdToRun(this);
+
+        grunt.config.set(watch.files, this.filesSrc);
+
+        grunt.config.set(watch.tasks, function () {
+            shell.exec(finalCmd);
+        });
     });
-  });
 };
